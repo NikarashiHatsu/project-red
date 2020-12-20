1eea9d094e5135809d2a337ba613502b6b49582a
NikarashiHatsu <yourlovelydev@gmail.com>
2020-12-20 22:56:26 +0700

    fix: default key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         Inertia::share([
             'errrors' => function() {
                 return Session::get('errors')
