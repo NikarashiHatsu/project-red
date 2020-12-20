d2c3a5ad10020eb316c3b53bd00c064e10c3cae3
NikarashiHatsu <yourlovelydev@gmail.com>
2020-12-20 22:20:44 +0700

    feat: upgraded tailwindcss to 2.0.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     purge: [
@@ -13,6 +14,26 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            
+            colors: {
+                red: colors.red,
+                orange: colors.orange,
+                amber: colors.amber,
+                yellow: colors.yellow,
+                lime: colors.lime,
+                green: colors.green,
+                emerald: colors.emerald,
+                teal: colors.teal,
+                cyan: colors.cyan,
+                lightBlue: colors.lightBlue,
+                blue: colors.blue,
+                indigo: colors.indigo,
+                violet: colors.violet,
+                purple: colors.purple,
+                fuchsia: colors.fuchsia,
+                pink: colors.pink,
+                rose: colors.rose
+            }
         },
     },
 
