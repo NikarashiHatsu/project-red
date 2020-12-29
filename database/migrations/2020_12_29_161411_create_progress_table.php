894152e6e16d3b5418487de022722bdf82218965
NikarashiHatsu <yourlovelydev@gmail.com>
2020-12-29 23:26:59 +0700

    feat: progress table migration

---
+++ database/migrations/2020_12_29_161411_create_progress_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_order_id');
+            $table->boolean('is_apk_created')->nullable();
+            $table->string('apk_directory')->nullable();
+            $table->boolean('is_published_on_google_play')->nullable();
+            $table->string('google_play_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('progress');
+    }
+}
