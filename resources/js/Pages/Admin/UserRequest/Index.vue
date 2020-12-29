84c0181f17fcfff1023f3ea1711cd44c393b79fe
NikarashiHatsu <yourlovelydev@gmail.com>
2020-12-29 22:36:00 +0700

    ui: added eye to toggle between show / hide list

--- resources/js/Pages/Admin/UserRequest/Index.vue
+++ resources/js/Pages/Admin/UserRequest/Index.vue
@@ -10,29 +10,47 @@
                 <div class="grid grid-cols-12 grid-flow-row gap-6">
                     <div class="col-span-12 sm:col-span-6 md:col-span-4">
                         <div class="bg-white border-l-4 border-yellow-400 rounded-none sm:rounded-lg shadow-lg">
-                            <h5 class="text-xl font-semibold p-4">Menunggu Persetujuan</h5>
+                            <div class="flex justify-between items-center">
+                                <h5 class="text-xl inline-flex font-semibold p-4">Menunggu Persetujuan</h5>
+                                <button v-if="!requested_hidden" @click="requested_hidden = true" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye"></i>
+                                </button>
+                                <button v-if="requested_hidden" @click="requested_hidden = false" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye-slash"></i>
+                                </button>
+                            </div>
                             <div v-if="requested_orders.length > 0">
                                 <user-request
                                     class="p-4 border-t"
                                     v-for="req in requested_orders"
+                                    :class="{ 'hidden': requested_hidden }"
                                     :key="req.id"
                                     :userData="req" />
                             </div>
-                            <div class="p-4 border-t" v-else>
+                            <div :class="{ 'hidden': requested_hidden }" class="p-4 border-t" v-else>
                                 Belum ada permintaan yang menunggu persetujuan
                             </div>
                         </div>
                         
-                        <div class="block md:hidden mt-6 bg-white border-l-4 border-red-400 rounded-none sm:rounded-lg shadow-lg">
-                            <h5 class="text-xl font-semibold p-4">Ditolak</h5>
+                        <div class="hidden sm:block md:hidden mt-6 bg-white border-l-4 border-red-400 rounded-none sm:rounded-lg shadow-lg">
+                            <div class="flex justify-between items-center">
+                                <h5 class="text-xl inline-flex font-semibold p-4">Ditolak</h5>
+                                <button v-if="!rejected_hidden" @click="rejected_hidden = true" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye"></i>
+                                </button>
+                                <button v-if="rejected_hidden" @click="rejected_hidden = false" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye-slash"></i>
+                                </button>
+                            </div>
                             <div v-if="rejected_orders.length > 0">
                                 <user-request
                                     class="p-4 border-t"
                                     v-for="req in rejected_orders"
+                                    :class="{ 'hidden': rejected_hidden }"
                                     :key="req.id"
                                     :userData="req" />
                             </div>
-                            <div class="p-4 border-t" v-else>
+                            <div :class="{ 'hidden': rejected_hidden }" class="p-4 border-t" v-else>
                                 Belum ada permintaan yang ditolak
                             </div>
                         </div>
@@ -40,31 +58,49 @@
 
                     <div class="col-span-12 sm:col-span-6 md:col-span-4">
                         <div class="bg-white border-l-4 border-green-400 rounded-none sm:rounded-lg shadow-lg">
-                            <h5 class="text-xl font-semibold p-4">Diterima</h5>
+                            <div class="flex justify-between items-center">
+                                <h5 class="text-xl inline-flex font-semibold p-4">Diterima</h5>
+                                <button v-if="!confirmed_hidden" @click="confirmed_hidden = true" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye"></i>
+                                </button>
+                                <button v-if="confirmed_hidden" @click="confirmed_hidden = false" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye-slash"></i>
+                                </button>
+                            </div>
                             <div v-if="confirmed_orders.length > 0">
                                 <user-request
                                     class="p-4 border-t"
                                     v-for="req in confirmed_orders"
+                                    :class="{ 'hidden': confirmed_hidden }"
                                     :key="req.id"
                                     :userData="req" />
                             </div>
-                            <div class="p-4 border-t" v-else>
+                            <div :class="{ 'hidden': confirmed_hidden }" class="p-4 border-t" v-else>
                                 Belum ada permintaan yang diterima
                             </div>
                         </div>
                     </div>
 
                     <div class="col-span-12 sm:col-span-6 md:col-span-4">
-                        <div class="hidden md:block bg-white border-l-4 border-red-400 rounded-lg shadow-lg">
-                            <h5 class="text-xl font-semibold p-4">Ditolak</h5>
+                        <div class="block sm:hidden md:block bg-white border-l-4 border-red-400 rounded-none sm:rounded-lg shadow-lg">
+                            <div class="flex justify-between items-center">
+                                <h5 class="text-xl inline-flex font-semibold p-4">Ditolak</h5>
+                                <button v-if="!rejected_hidden" @click="rejected_hidden = true" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye"></i>
+                                </button>
+                                <button v-if="rejected_hidden" @click="rejected_hidden = false" class="bg-white mr-4 inline-flex border border-gray-300 p-2 rounded-md">
+                                    <i class="fas fa-eye-slash"></i>
+                                </button>
+                            </div>
                             <div v-if="rejected_orders.length > 0">
                                 <user-request
                                     class="p-4 border-t"
                                     v-for="req in rejected_orders"
+                                    :class="{ 'hidden': rejected_hidden }"
                                     :key="req.id"
                                     :userData="req" />
                             </div>
-                            <div class="p-4 border-t" v-else>
+                            <div :class="{ 'hidden': rejected_hidden }" class="p-4 border-t" v-else>
                                 Belum ada permintaan yang ditolak
                             </div>
                         </div>
@@ -91,6 +127,9 @@
                 requested_orders: this.$page.requested_orders,
                 confirmed_orders: this.$page.confirmed_orders,
                 rejected_orders: this.$page.rejected_orders,
+                requested_hidden: false,
+                confirmed_hidden: false,
+                rejected_hidden: false,
             }
         }
     }
