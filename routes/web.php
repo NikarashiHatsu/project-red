0adc9e8810cbd92c1e3406a207a13d14d4a0378a
NikarashiHatsu <yourlovelydev@gmail.com>
2020-12-30 23:26:35 +0700

    feat: restrict admin for using owner's feature

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,13 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('/form_order', FormOrderController::class)->except(['create', 'edit', 'show', 'destroy']);
-    Route::resource('/layout_picker', LayoutPickerController::class)->except(['create', 'store', 'show', 'edit', 'destroy']);
-    Route::resource('/produk', ProductController::class)->except(['index', 'create', 'show', 'edit']);
-    Route::get('/pricing', [PricingController::class, 'index'])->name('pricing.index');
+    Route::group(['middleware' => 'owner'], function() {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('/form_order', FormOrderController::class)->except(['create', 'edit', 'show', 'destroy']);
+        Route::resource('/layout_picker', LayoutPickerController::class)->except(['create', 'store', 'show', 'edit', 'destroy']);
+        Route::resource('/produk', ProductController::class)->except(['index', 'create', 'show', 'edit']);
+        Route::get('/pricing', [PricingController::class, 'index'])->name('pricing.index');
+    });
 
     Route::group(['middleware' => 'admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
         Route::resource('/user_request', UserRequestController::class)->only(['index', 'show']);
