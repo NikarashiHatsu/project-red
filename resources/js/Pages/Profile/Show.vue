5df663ecad6b71c13f82d0d39fa405f9f58beff7
NikarashiHatsu <yourlovelydev@gmail.com>
2020-12-31 01:02:26 +0700

    fix: profile bug

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -1,42 +1,81 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Profile
-            </h2>
-        </template>
+    <div>
+        <app-layout v-if="$page.user.role == 'owner'">
+            <template #header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Profile
+                </h2>
+            </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <div v-if="$page.jetstream.canUpdateProfileInformation">
-                    <update-profile-information-form :user="$page.user" />
+            <div>
+                <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                    <div v-if="$page.jetstream.canUpdateProfileInformation">
+                        <update-profile-information-form :user="$page.user" />
+
+                        <jet-section-border />
+                    </div>
+
+                    <div v-if="$page.jetstream.canUpdatePassword">
+                        <update-password-form class="mt-10 sm:mt-0" />
+
+                        <jet-section-border />
+                    </div>
+
+                    <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
+                        <two-factor-authentication-form class="mt-10 sm:mt-0" />
+
+                        <jet-section-border />
+                    </div>
+
+                    <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
 
                     <jet-section-border />
+
+                    <delete-user-form class="mt-10 sm:mt-0" />
                 </div>
-
-                <div v-if="$page.jetstream.canUpdatePassword">
-                    <update-password-form class="mt-10 sm:mt-0" />
-
-                    <jet-section-border />
-                </div>
-
-                <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
-                    <two-factor-authentication-form class="mt-10 sm:mt-0" />
-
-                    <jet-section-border />
-                </div>
-
-                <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
-
-                <jet-section-border />
-
-                <delete-user-form class="mt-10 sm:mt-0" />
             </div>
-        </div>
-    </app-layout>
+        </app-layout>
+
+        <admin-app-layout v-if="$page.user.role == 'admin'">
+            <template #header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Profile
+                </h2>
+            </template>
+
+            <div>
+                <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                    <div v-if="$page.jetstream.canUpdateProfileInformation">
+                        <update-profile-information-form :user="$page.user" />
+
+                        <jet-section-border />
+                    </div>
+
+                    <div v-if="$page.jetstream.canUpdatePassword">
+                        <update-password-form class="mt-10 sm:mt-0" />
+
+                        <jet-section-border />
+                    </div>
+
+                    <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
+                        <two-factor-authentication-form class="mt-10 sm:mt-0" />
+
+                        <jet-section-border />
+                    </div>
+
+                    <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
+
+                    <jet-section-border />
+
+                    <delete-user-form class="mt-10 sm:mt-0" />
+                </div>
+            </div>
+        </admin-app-layout>
+    </div>
 </template>
 
 <script>
+    import AdminAppLayout from '@/Layouts/AdminAppLayout'
     import AppLayout from '@/Layouts/AppLayout'
     import DeleteUserForm from './DeleteUserForm'
     import JetSectionBorder from '@/Jetstream/SectionBorder'
@@ -49,6 +88,7 @@
         props: ['sessions'],
 
         components: {
+            AdminAppLayout,
             AppLayout,
             DeleteUserForm,
             JetSectionBorder,
