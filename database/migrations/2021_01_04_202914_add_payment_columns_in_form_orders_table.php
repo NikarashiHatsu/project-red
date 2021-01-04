7a128fd72b5053a58e289476da78e07f9ca7421e
NikarashiHatsu <yourlovelydev@gmail.com>
2021-01-05 03:46:49 +0700

    feat: ramp up the payment

--- database/migrations/2021_01_04_202914_add_payment_columns_in_form_orders_table.php
+++ database/migrations/2021_01_04_202914_add_payment_columns_in_form_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentsTable extends Migration
+class AddPaymentColumnsInFormOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreatePaymentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('payments', function (Blueprint $table) {
-            $table->id();
-            $table->string('session_id');
-            $table->string('url');
-            $table->timestamps();
+        Schema::table('form_orders', function (Blueprint $table) {
+            $table->string('sid')->nullable();
         });
     }
 
@@ -28,6 +25,8 @@ class CreatePaymentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payments');
+        Schema::table('form_orders', function (Blueprint $table) {
+            //
+        });
     }
 }
