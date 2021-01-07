0dfd77207d021d1b771e36912eb031fcb868ea4d
NikarashiHatsu <yourlovelydev@gmail.com>
2021-01-08 00:58:56 +0700

    production stage 5

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,11 +11,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') . '?v=4' }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') . '?v=5' }}">
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') . '?v=4' }}" defer></script>
+        <script src="{{ mix('js/app.js') . '?v=5' }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
